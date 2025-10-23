7d44379057d8c9a59727231bb41e248240565a54
KarimLabidProf <karim.labidi@esprit.tn>
2025-10-23 13:34:08 +0000

    Add French greeting and fix quote in script.ps1
    Added a new line to greet in French and fixed a quote.

--- script.ps1
+++ script.ps1
@@ -3,4 +3,6 @@ param (
     [string]$Env = "Development"
 )
 
-Write-Host "Hello from PowerShell! Running in $Env environment.“
+Write-Host "Hello from PowerShell! Running in $Env environment."
+
+Write-Host " Première exécution de github Action
