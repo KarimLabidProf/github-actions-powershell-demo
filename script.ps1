d5e57a497a3ec85f0d8deedadd7d98d4ab74fef8
KarimLabidProf <karim.labidi@esprit.tn>
2025-10-23 13:53:40 +0000

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -6,3 +6,10 @@ param (
 Write-Host "Hello from PowerShell! Running in $Env environment."
 
 Write-Host " Première exécution de github Action"
+
+$Username = "monuser"
+$Password = "MonPasswordSuperSecret123!"  # <-- VULN ICI : Secret en clair !
+
+# Création credential
+$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential($Username, $SecurePassword)
